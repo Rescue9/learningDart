6351ce336870bb2370c7ac90eb06fa7dfa63331b
Rescue9 <andrew.buskov@gmail.com>
2026-04-04 16:45:57 +0000

    Define Relationships with Classes

--- dartpedia/cli/bin/cli.dart
+++ dartpedia/cli/bin/cli.dart
@@ -1,61 +1,8 @@
-import 'dart:io';
-import 'package:http/http.dart' as http;
+import 'package:command_runner/command_runner.dart';
 
 const version = '0.0.1';
 
 void main(List<String> arguments) {
-  if (arguments.isEmpty || arguments.first == 'help') {
-    printUsage();
-  } else if (arguments.first == 'version') {
-    print('Dartpedia CLI version $version');
-  } else if (arguments.first == 'wikipedia') {
-    final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
-    searchWikipedia(inputArgs);
-  } else {
-    printUsage(); // catchall for unrecognized commands
-  }
-}
-
-void searchWikipedia(List<String>? arguments) async {
-  final String articleTitle;
-
-  // if user didn't pass arguments, request article title.
-  if (arguments == null || arguments.isEmpty) {
-    print('Please provide an article title.');
-    final inputFromStdin = stdin.readLineSync();
-    if (inputFromStdin == null || inputFromStdin.isEmpty) {
-      print('No article title provided. Exiting.');
-      return;
-    }
-    articleTitle = inputFromStdin;
-  } else {
-    // otherwise, hoin all arguments into a single string
-    articleTitle = arguments.join(' ');
-  }
-
-  print('Looking up articles about "$articleTitle". Please wait.');
-
-  var articleContent = await getWikipediaArticle(articleTitle);
-  print(articleContent); // Print article, Raw JSON currently
-}
-
-void printUsage() {
-  print(
-    "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'",
-  );
-}
-
-Future<String> getWikipediaArticle(String articleTitle) async {
-  final url = Uri.https(
-    'en.wikipedia.org',
-    'api/rest_v1/page/summary/$articleTitle',
-  );
-  final response = await http.get(url);
-
-  if (response.statusCode == 200) {
-    return response.body;
-  }
-
-  // error message
-  return 'Error: Failed to fetch article "$articleTitle". Status Code: ${response.statusCode}';
+  var commandRunner = CommandRunner()..addCommand(HelpCommand());
+  commandRunner.run(arguments);
 }
