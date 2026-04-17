b8a783c4606aa4c62aef6bbc9ad23b79ab8eea86
Rescue9 <andrew.buskov@gmail.com>
2026-04-17 13:18:50 -0500

    Finished Handle User Input The TextField Widget step

--- birdle/lib/main.dart
+++ birdle/lib/main.dart
@@ -10,12 +10,14 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-        body: Center(
-          child: Tile('A', HitType.partial),
+        appBar: AppBar(
+          title: Align(alignment: Alignment.centerLeft, child: Text('Birdle')),
         ),
+
+        backgroundColor: Colors.blueGrey,
+        body: Center(child: GamePage()),
       ),
     );
   }
@@ -34,19 +36,70 @@ class Tile extends StatelessWidget {
       height: 60,
       decoration: BoxDecoration(
         border: Border.all(color: Colors.grey.shade300),
-        color: switch (hitType){
+        color: switch (hitType) {
           HitType.hit => Colors.green,
           HitType.partial => Colors.yellow,
           HitType.miss => Colors.grey,
           _ => Colors.white,
-        }
+        },
       ),
       child: Center(
         child: Text(
           letter.toUpperCase(),
           style: Theme.of(context).textTheme.titleLarge,
+        ),
+      ),
+    );
+  }
+}
+
+class GamePage extends StatelessWidget {
+  GamePage({super.key});
+  // This object is par  of the game.dart file
+  // It manages wordle logic, and is outside the scope of this tutorial
+  final Game _game = Game();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        spacing: 5.0,
+        children: [
+          for (var guess in _game.guesses)
+          Row(spacing: 5.0,
+              children: [
+                for (var letter in guess) Tile(letter.char, letter.type)
+              ],)
+        ],
+      ),
+    );
+  }
+}
+
+class GuessInput extends StatelessWidget {
+  GuessInput({super.key, required this.onSubmitGuess});
+
+  final void Function(String) onSubmitGuess;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              maxLength: 5,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(35))
+                )
+              ),
+            ),
+          )
         )
-      )
+      ],
     );
   }
 }
