bb1d32eaf3b52bd236aecca6c7f78ec6c68790be
Rescue9 <andrew.buskov@gmail.com>
2026-04-18 15:00:42 -0500

    Completed State in Flutter Apps On a side note... IT WORKS! I had to add the android internet permission, but building worked perfectly after that and it runs on my device beautifully. WOOT!

--- wikipedia_reader/lib/main.dart
+++ wikipedia_reader/lib/main.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+
+import 'summary.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,11 +15,139 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(home: ArticleView());
+  }
+}
+
+class ArticleModel {
+  Future<Summary> getRandomArticleSummary() async {
+    final uri = Uri.https(
+      'en.wikipedia.org',
+      '/api/rest_v1/page/random/summary',
+    );
+    final response = await get(uri);
+
+    if (response.statusCode != 200) {
+      throw HttpException('Failed to update resource');
+    }
+
+    return Summary.fromJson(jsonDecode(response.body));
+  }
+}
+
+class ArticleViewModel extends ChangeNotifier {
+  final ArticleModel model;
+  Summary? summary;
+  String? errorMessage;
+  bool loading = false;
+
+  ArticleViewModel(this.model) {
+    getRandomArticleSummary();
+  }
+
+  Future<void> getRandomArticleSummary() async {
+    loading = true;
+    notifyListeners();
+    try {
+      summary = await model.getRandomArticleSummary();
+      print('Article loaded: ${summary!.titles.normalized}'); // Temporary
+      errorMessage = null; // Clear any previous errors.
+    } on HttpException catch (error) {
+      print('Error loading article: ${error.message}'); // Temporary
+      errorMessage = error.message;
+      summary = null;
+    }
+    loading = false;
+    notifyListeners();
+  }
+}
+
+class ArticleView extends StatelessWidget {
+  ArticleView({super.key});
+
+  final viewModel = ArticleViewModel(ArticleModel());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Wikipedia Flutter'), actions: []),
+      body: ListenableBuilder(
+        listenable: viewModel,
+        builder: (context, child) {
+          return switch ((
+            viewModel.loading,
+            viewModel.summary,
+            viewModel.errorMessage,
+          )) {
+            (true, _, _) => CircularProgressIndicator(),
+            (false, _, String message) => Center(child: Text(message)),
+            (false, null, null) => Center(
+              child: Text('An unknown error has occurred'),
+            ),
+            // The summary must be non-null in this switch case.
+            (false, Summary summary, null) => ArticlePage(
+              summary: summary,
+              nextArticleCallback: viewModel.getRandomArticleSummary,
+            ),
+          };
+        },
+      ),
+    );
+  }
+}
+
+class ArticlePage extends StatelessWidget {
+  const ArticlePage({
+    super.key,
+    required this.summary,
+    required this.nextArticleCallback,
+  });
+
+  final Summary summary;
+  final VoidCallback nextArticleCallback;
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          ArticleWidget(summary: summary),
+          ElevatedButton(
+            onPressed: nextArticleCallback,
+            child: Text('Next random article'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ArticleWidget extends StatelessWidget {
+  const ArticleWidget({super.key, required this.summary});
+
+  final Summary summary;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        spacing: 10.0,
+        children: [
+          if (summary.hasImage) Image.network(summary.originalImage!.source),
+          Text(
+            summary.titles.normalized,
+            overflow: TextOverflow.ellipsis,
+            style: TextTheme.of(context).displaySmall,
+          ),
+          if (summary.description != null)
+            Text(
+              summary.description!,
+              overflow: TextOverflow.ellipsis,
+              style: TextTheme.of(context).bodySmall,
+            ),
+          Text(summary.extract),
+        ],
       ),
     );
   }
